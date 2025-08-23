531d2815853fe45a90d7b63aac3f98388ebaee3a
Sagnik Bankura <bankura.sagnik@gmail.com>
2025-08-23 14:36:04 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
 // client/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // The official plugin
+import tailwindcss from '@tailwindcss/vite' 
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // This handles everything for you!
+    tailwindcss(),
   ],
   server: {
     proxy: {
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const colors = require('colors');
+const path = require('path');
 const connectDb = require('./config/db');
 
 dotenv.config();
@@ -18,6 +19,12 @@ app.use(cors());
 app.use('/api/v1/users', require('./routes/userRoute'));
 
 app.use('/api/v1/transactions', require('./routes/transactionRoute'));
+
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 
